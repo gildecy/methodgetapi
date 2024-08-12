5972d5b713a4dc173a8c56db7a2e07b861774ea2
Gildecy de Jesus Sá <101933897+gildecy@users.noreply.github.com>
2024-08-11 23:57:03 -0300

    Update methodget.js

--- methodget.js
+++ methodget.js
@@ -1,34 +1,33 @@
 const formatDate = (date)=>{
-  if(date === null) return "-"
-  const data = new Date(date);
-  return new Intl.DateTimeFormat('pt-BR', {timeZone: 'UTC'}).format(data);
-}
-
-async function fetchDados() {
-    try {
-      const response = await fetch('https://pool-api-alpha.vercel.app/api/v1/pool/');
-      const dados = await response.json();
-      const lista = document.getElementById('dados-lista');
-      console.log(dados.data)
-      let html = ""
-      for (let index = 0; index < dados.data.length; index++) {
-        html += "<tr>";
-        html += "<td>" + dados.data[index].pool_id+ "</td>";
-        html += "<td>" + dados.data[index].nome+ "</td>";
-        html += "<td>" + dados.data[index].algicida+ "</td>";
-        html += "<td>" + dados.data[index].barrilha+ "</td>";
-        html += "<td>" + dados.data[index].clarificante+ "</td>";
-        html += "<td>" + dados.data[index].cloro+ "</td>";
-        html += "<td>" + formatDate(dados.data[index].date)+ "</td>";
-        html += "</tr>";
-      }
-
-      lista.innerHTML = html;
-
-    } catch (error) {
-      console.error('Erro ao buscar dados:', error);
-    }
+    if(date === null) return "-"
+    const data = new Date(date);
+    return new Intl.DateTimeFormat('pt-BR', {timeZone: 'UTC'}).format(data);
   }
   
-  document.addEventListener('DOMContentLoaded', fetchDados);
+  async function fetchDados() {
+      try {
+        const response = await fetch('https://pool-api-alpha.vercel.app/api/v1/pool/');
+        const dados = await response.json();
+        const lista = document.getElementById('dados-lista');
+        console.log(dados.data)
+        let html = ""
+        for (let index = 0; index < dados.data.length; index++) {
+          html += "<tr>";
+          html += "<td>" + dados.data[index].pool_id+ "</td>";
+          html += "<td>" + dados.data[index].nome+ "</td>";
+          html += "<td>" + dados.data[index].algicida+ "</td>";
+          html += "<td>" + dados.data[index].barrilha+ "</td>";
+          html += "<td>" + dados.data[index].clarificante+ "</td>";
+          html += "<td>" + dados.data[index].cloro+ "</td>";
+          html += "<td>" + formatDate(dados.data[index].date)+ "</td>";
+          html += "</tr>";
+        }
   
+        lista.innerHTML = html;
+  
+      } catch (error) {
+        console.error('Erro ao buscar dados:', error);
+      }
+    }
+    
+    document.addEventListener('DOMContentLoaded', fetchDados);
